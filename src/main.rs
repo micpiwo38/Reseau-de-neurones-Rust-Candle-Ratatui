ea463850f867d50eeae8a97a281f3b620c1005b3
qiwebDev <qiweb.formation@gmail.com>
2026-07-19 14:17:00 +0200

    Signed-off-by: qiwebDev <qiweb.formation@gmail.com>Base du programme

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ pub mod model;
 use crate::app::{App};
 
 fn main() -> io::Result<()> {
-    App::run().expect("Erreur de création de l'application MIC_AI !");
+    App::run().expect("Erreur de création de l'application MIC_AI !!!");
     println!("Merci d'avoir utilisé le LLM MIC_IA !");
 
     Ok(())
